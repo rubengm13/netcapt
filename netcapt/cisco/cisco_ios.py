790e82875e12ecf1a04e82072290618fa1fd0cd8
Ruben <66105177+rubengm13@users.noreply.github.com>
2020-11-28 16:52:12 -0800

    Need to determine if this version of gather_arp should be defaulted to cisco devices. or because they are all different it should be here. Do we need a default gather_arp? for all Cisco Devices?

--- netcapt/cisco/cisco_ios.py
+++ netcapt/cisco/cisco_ios.py
@@ -4,3 +4,24 @@ from netcapt.cisco.cisco import CiscoNetworkDevice
 class CiscoIosDevice(CiscoNetworkDevice):
     def __str__(self):
         return "<Cisco IOS Device> host: {self.host}".format(self=self)
+
+    def gather_arp(self):
+        """
+        Captures arp information and utilizing the vrf data it parses the
+        output to prepare it for extraction to WB.
+        """
+        vrf_list = self.get_vrf_names()
+        arp_list = list()
+        for vrf in vrf_list:
+            if vrf != "global":
+                output = self.show_arp_vrf(vrf)
+            else:
+                output = self.show_arp()
+            if isinstance(output, list):
+                for arp in output:
+                    arp["vrf"] = vrf
+                    arp_list.append(arp)
+            else:
+                arp = {'vrf': vrf, 'address': "No ARP Data Found"}
+                arp_list.append(arp)
+        return arp_list
