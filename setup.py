b1d63d50a5c91ccb15de462fe24878e3876722c7
Ruben <66105177+rubengm13@users.noreply.github.com>
2021-01-25 12:42:10 -0800

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netcapt',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     url='https://github.com/rubengm13/netcapt',
     license='GNU GPL',
