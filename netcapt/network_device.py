f44fae01738817eb3bd9ccb8bf84c1dfbcda9534
Ruben <66105177+rubengm13@users.noreply.github.com>
2021-02-05 17:24:00 -0800

    Added a Verbose handler, added place holder for Gather methods. Added Extreme EXOS to index file for future testing. Cleaned up CiscoIOS gather_cdp, no more static template for this.

--- netcapt/network_device.py
+++ netcapt/network_device.py
@@ -1,6 +1,11 @@
 import netmiko
 from unipath import Path
 import os
+import traceback
+
+
+class InitiatingConnectionException(Exception):
+    pass
 
 
 # TODO: Do we need to derive this class from the Netmiko Class?
@@ -12,18 +17,22 @@ class NetworkDevice(object):
     def __init__(
             self,
             auto_connect=False,
+            verbose=False,
             **kwargs
     ):
         # Added the netmiko class here
         # Note it will start the connection if auto_connect is set to True, Defaulted to False.
-        self.connection = netmiko.ConnectHandler(auto_connect=auto_connect, **kwargs)
+        self.connection = netmiko.ConnectHandler(auto_connect=auto_connect, verbose=verbose, **kwargs)
+
+        self.host = kwargs['host']
+        self.verbose = verbose
         self.model = None
         self.version = None
         self.serial_number = None
         self.hostname = None
 
     def __str__(self):
-        return "<%s | host: %s>"% (self.classname, self.connection.host)
+        return "<%s | host: %s>"% (self.classname, self.host)
 
     # Gather Functions
     # Most are Empty Place holders for respective Device Gathers
@@ -32,6 +41,15 @@ class NetworkDevice(object):
         Gather the Version output, will return after it is parsed using TextFSM
         :return: List with Dictionary values
         """
+        pass
+
+    def gather_arp(self):
+        pass
+
+    def gather_mac(self):
+        pass
+
+    def gather_interfaces(self):
         pass
 
     def gather_cdp(self):
@@ -46,13 +64,19 @@ class NetworkDevice(object):
     def gather_lldp(self):
         pass
 
-    def gather_arp(self):
+    def gather_route(self):
+        pass
+
+    def gather_bgp(self):
+        pass
+
+    def gather_inventory(self):
         pass
 
     def gather_commands(self, command_list):
         return_dict = dict()
         for command in command_list:
-            return_dict[command_list] = self.send_command(command)
+            return_dict[command] = self.send_command(command)
         return return_dict
 
     # Connection Commands
@@ -64,6 +88,24 @@ class NetworkDevice(object):
         if not self.connection.is_alive():
             self.connection.establish_connection()
             self.connection.session_preparation()
+
+    def start_connect_w_retry(self, max_attempts=3):
+        """
+        Start Connection and handle attempts, This method will allow connection reattempts
+        :param max_attempts: int default is 3
+        """
+        self.verbose_msg('Starting Connection')
+        attempt = 0
+        for attempt in range(max_attempts):
+            try:
+                self.start_connection()
+                self.connection.session_preparation()
+                return
+            except Exception as e:
+                self.verbose_msg('LOGIN FAILURE: Attempt to Establish Connection Failed, Attempt: ' + str(attempt))
+        raise InitiatingConnectionException(
+            'LOGIN FAILURE: Attempt to Establish Connection Failed, Attempt: ' + str(attempt)
+        )
 
     def end_connection(self):
         self.connection.disconnect()
@@ -104,6 +146,12 @@ class NetworkDevice(object):
     def classname(self):
         return self.__class__.__name__
 
+    def verbose_msg(self, msg):
+        if self.verbose:
+            line1 = str(self.host)
+            if len(line1) < 15:
+                line1 += (15 - len(line1)) * " "
+            print(self, "verbose:", msg)
 
 def test_print():
     print("This is a test of test broadcast system")
