3fe2a665a962944b087749e0deeec9fda8f985cc
Ruben <66105177+rubengm13@users.noreply.github.com>
2021-02-08 18:51:24 -0800

    Added a Gather AP function, Captures the show ap summary output and combines it with each individual show ap config {name} output.

--- netcapt/cisco/cisco_wlc.py
+++ netcapt/cisco/cisco_wlc.py
@@ -7,6 +7,25 @@ class TextFsmParseIssue(Exception):
 
 
 class CiscoWlcDevice(CiscoNetworkDevice):
+
+    def gather_ap(self):
+        """
+        Gather the AP Summary and combine the data with each AP Gather
+        :return:
+        """
+        ap_sum = self.send_command('show ap summary', use_textfsm=True)
+        for ap in ap_sum:
+            ap_config = self.send_command('show ap config general ' + ap['ap_name'], use_textfsm=True)
+            if len(ap_config) == 1:
+                ap.update(ap_config[0])
+            else:
+                raise ValueError(
+                    'Command show ap config general ' + ap['ap_name'] +
+                    'has more then one entry verify output with TextFSM Template'
+                )
+        return ap_sum
+
+
     # TODO: Consider if we need to override the cmd_verify per command or just here.
     # def send_command(self, command, cmd_verify=False, **kwargs):
     #     return self.connection.send_command(command_string=command, )
