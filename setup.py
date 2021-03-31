81fe22548c063cc1692bb44a354eef0d9ae88b4d
rubengm13 <rubeng318@ucla.edu>
2021-03-30 19:09:42 -0700

    Releasing as Version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netcapt',
-    version='0.1.2',
+    version='1.0.0',
     packages=find_packages(),
     url='https://github.com/rubengm13/netcapt',
     license='GNU GPL',
